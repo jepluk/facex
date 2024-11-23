0ad77f2a9932d60b4439878fa87c3bbc84e9bcbf
jepluk <khoirulazam787@gmail.com>
2024-11-23 12:53:22 +0700

    new release

---
+++ facex/cli.py
@@ -0,0 +1,44 @@
+import argparse, sqlite3
+from . import config, scrape_token
+
+def main():
+    arg = argparse.ArgumentParser(description='Beautiful Facebook Bruteforce Attack.')
+    sub = arg.add_subparsers(title='command', dest='command', required=True)
+
+    useragent = sub.add_parser('set', help='Setup')
+    useragent.add_argument('-ua', '--useragents', help='Set useragents to the database for Bruteforce Attack. set None for use random')
+    useragent.add_argument('-c', '--cookie', help='Set cookie and tokeb to the database.')
+
+    parse = arg.parse_args()
+    print(parse)
+    match parse.command:
+        case 'set':
+            if parse.useragents:
+                with sqlite3.connect(config.DB_PATH) as db:
+                    cursor = db.cursor()
+                    cursor.execute('DELETE FROM useragents')
+                    cursor.execute('INSERT INTO useragents (ua) VALUES (?)', (parse.useragents,))
+
+                    db.commit()
+
+                print('\n[ INFO! ] Successfull set useragents.\nValue: '+ str(parse.useragents))
+
+            if parse.cookie:
+                with sqlite3.connect(config.DB_PATH) as db:
+                    cursor = db.cursor()
+                    cursor.execute('DELETE FROM user')
+                    
+                    token_out = scrape_token.Scrape(cookie=parse.cookie).oauth()
+                    if 'EAAB' in str(token_out):
+                        print(f'\n[ INFO! ] Successfull set cookie and token!\nCookie: {parse.cookie}\nToken: {token_out}')
+                        cursor.execute('INSERT INTO user (cookie, token) VALUES (?,?)', (parse.cookie, token_out))
+                    else:
+                        print(f'\n[ WARN! ] Faillure setup cookie and token.\nPlease use another cookie.')
+
+                    db.commit()
+
+
+
+
+main()
+
---
+++ facex/scrape_token.py
@@ -0,0 +1,18 @@
+import requests
+
+class Scrape:
+    def __init__(self, cookie: str=None, session: object=None) -> None:
+        self.ses = session if session else requests.session()
+        self.ses.cookies.update({'cookie': cookie}) if cookie is not None else None 
+
+    def oauth(self) -> (str, bool):
+        hdx = str(self.ses.get('https://www.facebook.com/x/oauth/status?client_id=124024574287414&wants_cookie_data=true&origin=1&input_token=&sdk=joey', headers={'Accept-Language': 'id,en;q=0.9','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36','Referer': 'https://www.instagram.com/','Host': 'www.facebook.com','Sec-Fetch-Mode': 'cors','Accept': '*/*','Connection': 'keep-alive','Sec-Fetch-Site': 'cross-site','Sec-Fetch-Dest': 'empty','Origin': 'https://www.instagram.com','Accept-Encoding': 'gzip, deflate'}).headers)
+
+        if 'EAAB' in hdx:
+            return re.search(r'"(EAAB.*?)",', hdx).group(1)
+
+
+
+
+
+
