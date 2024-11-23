a107da253a7f8b918e89faf3cfdd364dd633a552
jepluk <khoirulazam787@gmail.com>
2024-11-23 12:59:13 +0700

    new release

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+import os, shutil
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+
+        src_file = os.path.join(os.path.dirname(__file__), 'my_package', 'file_yang_ingin_disalin')
+        dest_dir = '/data/data/com.termux/files/usr/bin/'
+        os.makedirs(dest_dir, exist_ok=True)
+        shutil.copy(src_file, dest_dir)
 
 setup(
     name='facex',
@@ -11,5 +22,8 @@ setup(
         'console_scripts': [
             'facex=facex.cli:main',
         ],
+    },
+    cmdclass={
+        'install': PostInstallCommand,
     },
 )
